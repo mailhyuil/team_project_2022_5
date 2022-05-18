87e70dc87e7b04a950a1683652162242faa7ded6
minuk0506 <leeminuk9238@daum.net>
2022-05-18 16:47:51 +0900

    lmu0518

--- js/academy-info.js
+++ js/academy-info.js
@@ -1,20 +1,18 @@
-const button = document.querySelector(".basket-button");
 const map_button = document.querySelector(".map"); // map버튼
 const aca_wrapper = document.querySelector(".section"); // info
+const info_wrapper = document.querySelector(".put-in-button");
 
 map_button.addEventListener("click", () => {
+  console.log("hi");
   // map누르면 info 나오게
-  aca_wrapper.classList.add("click");
+  console.log(basket_wrapper.className);
+  if (basket_wrapper.className === "wrapper click") {
+    return;
+  } else if (basket_wrapper.className === "wrapper") {
+    aca_wrapper.classList.toggle("click");
+  }
 });
-
-aca_wrapper.childNodes[1].addEventListener("click", () => {
-  // 장바구니 숨기기
-  aca_wrapper.classList.remove("click");
-  aca_wrapper.style.display = "flex";
-});
-
-basket_wrapper.childNodes[1].addEventListener("click", () => {
-  // 장바구니 숨기기
-  basket_wrapper.classList.remove("click");
-  aca_wrapper.style.display = "flex";
-});
+// bascket.js
+// basket_wrapper click 에 aca_wrapper add
+// button click 에 aca_wrapper remove
+// 들어가있음
