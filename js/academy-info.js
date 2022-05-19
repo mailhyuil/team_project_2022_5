b48eca1242864a9a964d8da87573f180466e5fa0
mailhyuil <mailhyuil@gmail.com>
2022-05-19 22:51:01 +0900

    update

--- js/academy-info.js
+++ js/academy-info.js
@@ -2,14 +2,16 @@ const map_button = document.querySelectorAll(".map-btn"); // map버튼
 const aca_wrapper = document.querySelector(".section"); // info
 const put_in_btn = document.querySelector(".put-in-btn");
 const aside = document.querySelector(".aside");
-const btn = document.querySelector(".btn");
+const aside_btn = document.querySelector(".aside_btn");
 const btnOpen = document.querySelector(".btn_open");
+const left_arrow = aside.querySelector(".xi-angle-left.xi-2x");
+const right_arrow = aside.querySelector(".xi-angle-right.xi-2x");
 
 for (let btn of map_button) {
   btn.addEventListener("click", () => {
-    console.log(put_in_btn.className);
+
     // map누르면 info 나오게
-    console.log(basket_wrapper.className);
+
     if (basket_wrapper.className === "wrapper click") {
       return;
     } else if (basket_wrapper.className === "wrapper") {
@@ -17,11 +19,11 @@ for (let btn of map_button) {
     }
 
     if (aca_wrapper.className === "section click") {
-      button.style.left = "6rem";
-      button.style.right = null;
+      basket_button.style.left = "6rem";
+      basket_button.style.right = null;
     } else {
-      button.style.left = null;
-      button.style.right = "5rem";
+      basket_button.style.left = null;
+      basket_button.style.right = "5rem";
     }
 
     aside.classList.add("click");
@@ -29,17 +31,33 @@ for (let btn of map_button) {
     if (aca_wrapper.className === "section") aside.classList.remove("click");
   });
 }
+
+
 put_in_btn.addEventListener("click", () => {
   aca_wrapper.classList.remove("click");
   aside.classList.remove("click");
+
+  if (aca_wrapper.className === "section") {
+
+    basket_button.style.left = null;
+    basket_button.style.right = "5rem";
+  } else {
+    basket_button.style.left = "6rem";
+    basket_button.style.right = null;
+  }
+
 });
 
-btn.addEventListener("click", () => {
+
+aside_btn.addEventListener("click", () => {
   aside.classList.toggle("click");
+  if (aside.className === "aside click") {
+    left_arrow.style.display = "none"
+    right_arrow.style.display = "inline-block"
+  } else {
+    left_arrow.style.display = "inline-block"
+    right_arrow.style.display = "none"
+  }
   if (aca_wrapper.className === "section click")
     aca_wrapper.classList.remove("click");
 });
-// bascket.js
-// basket_wrapper click 에 aca_wrapper add
-// button click 에 aca_wrapper remove
-// 들어가있음
--- js/basket.js
+++ js/basket.js
@@ -1,8 +1,32 @@
-const button = document.querySelector(".basket-button"); // 장바구니 버튼
+const basket_button = document.querySelector(".basket-button"); // 장바구니 버튼
 const basket_wrapper = document.querySelector(".wrapper"); // 장바구니
 const subs = document.querySelectorAll(".sub"); // 빼기 버튼
 const sub_wrapper = document.querySelector(".sub-wrapper");
 const count = document.querySelector(".count"); // 장바구니 카운트
+const $sum = document.querySelector(".sum");
+let classes = document.querySelectorAll(".classes");
+
+let sum = 0;
+
+
+for (let c of classes) {
+  c.addEventListener("click", (e) => {
+    // 장바구니 숨기기
+    if (e.target.className !== "sub") {
+      basket_wrapper.classList.remove("click");
+      basket_button.style.display = "flex";
+
+      aca_wrapper.classList.add("click");
+    }
+  })
+}
+
+
+for (let c of classes) {
+  let intPayment = Number.parseInt(c.dataset.expense);
+  sum += intPayment;
+}
+$sum.innerHTML = `총액 : ${sum}원/월`;
 
 for (let node of sub_wrapper.childNodes) {
   // 공백 텍스트 없애기
@@ -17,24 +41,46 @@ if (sub_wrapper.hasChildNodes()) {
   count.textContent = sub_wrapper.childNodes.length;
 }
 
-button.addEventListener("click", () => {
+basket_button.addEventListener("click", () => {
   // 버튼 누르면 장바구니 display=none으로
   basket_wrapper.classList.add("click");
-  button.style.display = "none";
+  basket_button.style.display = "none";
   aca_wrapper.classList.remove("click");
 });
 
 basket_wrapper.childNodes[1].addEventListener("click", () => {
   // 장바구니 숨기기
   basket_wrapper.classList.remove("click");
-  button.style.display = "flex";
+  basket_button.style.display = "flex";
 
   aca_wrapper.classList.add("click");
+
+  if (aca_wrapper.className === "section") {
+
+    basket_button.style.left = null;
+    basket_button.style.right = "5rem";
+  } else {
+    basket_button.style.left = "6rem";
+    basket_button.style.right = null;
+  }
+
 });
 
 for (let sub of subs) {
   sub.addEventListener("click", (e) => {
+
+
     sub_wrapper.removeChild(e.target.parentNode); // 장바구니 빼기
+
+    classes = document.querySelectorAll(".classes");
+    sum = 0;
+    console.log(classes);
+    for (let c of classes) {
+      let intPayment = Number.parseInt(c.dataset.expense);
+      sum += intPayment;
+    }
+    $sum.innerHTML = `총액 : ${sum}원/월`;
+
     if (!sub_wrapper.hasChildNodes()) {
       // 장바구니 내용이 없으면 비었다고 표시
       const h1 = document.createElement("h1");
@@ -45,5 +91,7 @@ for (let sub of subs) {
       count.style.display = "inline-block";
       count.textContent = sub_wrapper.childNodes.length;
     }
+
   });
 }
+
